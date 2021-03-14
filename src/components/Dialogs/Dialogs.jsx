38b13ca80a0ed830a0aa8f6bfa53438b68bafc19
Forgotten Broth <kuckklux@gmail.com>
2021-03-14 19:33:16 +0300

    Unit 21 Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import s from './Dialogs.module.css';
+import React from "react";
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-    return (
-        <div>
-            Dialogs
-        </div>
-    )
-}
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + '' +s.active}>cth</div>
+        <div className={s.dialog}>shbmnk</div>
+        <div className={s.dialog}>fv</div>
+        <div className={s.dialog}>Дмитрий Юшин</div>
+        <div className={s.dialog}>Аркадий Давидович</div>
+        <div className={s.dialog}>Jonyol47</div>
+      </div>
+
+      <div className={s.messages}>
+        <div className={s.message}>Братишка</div>
+        <div className={s.message}>Я тебе покушать принес</div>
+        <div className={s.message}>Угощайся</div>
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
